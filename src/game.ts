b76f24a5539c0305aaa8150c3b3b157f3e86df2e
tmacha <tobias-macha@gmx.de>
2019-09-25 14:48:13 +0200

    added floor and ball model
    .glb export + .blend project file
    (decentraland uses xz as ground plane and y as up axis; origin is always centered in volume on the lowest surface)

--- src/game.ts
+++ src/game.ts
@@ -1,3 +1,17 @@
 import utils from "../node_modules/decentraland-ecs-utils/index"
 
+let floor=new Entity()
+floor.addComponent(new GLTFShape("models/floor.glb"))
+floor.addComponent(new Transform({
+  position: new Vector3(8,0,24)
+  //rotation:
+}))
+let ball=new Entity()
+ball.addComponent(new GLTFShape("models/ball.glb"))
+ball.addComponent(new Transform({
+  position: new Vector3(8,0.75,24)
+  //rotation:
+}))
 
+engine.addEntity(ball)
+engine.addEntity(floor)
