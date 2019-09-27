792e48d2486b4a89f0b74099f005f05d031d4853
tmacha <tobias-macha@gmx.de>
2019-09-27 15:05:40 +0200

    added flappy bird environment

--- src/game.ts
+++ src/game.ts
@@ -1,17 +1,87 @@
 import utils from "../node_modules/decentraland-ecs-utils/index"
 
-let floor=new Entity()
-floor.addComponent(new GLTFShape("models/floor.glb"))
-floor.addComponent(new Transform({
-  position: new Vector3(8,0,24)
-  //rotation:
-}))
-let ball=new Entity()
-ball.addComponent(new GLTFShape("models/ball.glb"))
-ball.addComponent(new Transform({
-  position: new Vector3(8,0.75,24)
+//functions
+
+function randomIntFromInterval(min, max) { // min and max included 
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+//input handlers
+
+const input = Input.instance
+
+// button down event e
+input.subscribe("BUTTON_DOWN", ActionButton.PRIMARY, false, e => {
+  let height=cube.getComponent(Transform).position.y
+  cube.getComponent(Transform).position.set(8,height+1,14)
+  
+})
+// button down event f
+input.subscribe("BUTTON_DOWN", ActionButton.SECONDARY, false, e => {
+  let height=cube.getComponent(Transform).position.y
+  cube.getComponent(Transform).position.set(8,height+1,14)
+})
+
+//objects in scene
+
+let cube=new Entity()
+cube.addComponent(new GLTFShape("models/cube.glb"))
+cube.addComponent(new Transform({
+  position: new Vector3(8,5,14)
   //rotation:
 }))
 
-engine.addEntity(ball)
-engine.addEntity(floor)
+let roof=new Entity()
+roof.addComponent(new GLTFShape("models/roof.glb"))
+roof.addComponent(new Transform({
+  position: new Vector3(16,0,8)
+  //rotation:
+}))
+
+let collider=new Entity()
+collider.addComponent(new GLTFShape("models/collider.glb"))
+collider.addComponent(new Transform({
+  position: new Vector3(16,0,8)
+  //rotation:
+}))
+
+let obstacle = new Entity()
+obstacle.addComponent(new GLTFShape("models/obstacle.glb"))
+obstacle.addComponent(new Transform({
+  scale: new Vector3(1,10,1),
+  position: new Vector3(32,-3,14)
+}))
+
+
+
+//systems
+
+export class MoveObstacle implements ISystem {
+  update() {
+    let transform = obstacle.getComponent(Transform)
+    let distance = Vector3.Left().scale(0.1)
+    transform.translate(distance)
+  }
+}
+
+export class MoveBird implements ISystem {
+  update() {
+    let transform = cube.getComponent(Transform)
+    let current=cube.getComponent(Transform).position.y
+    if(current>0){
+    transform.position.y-=0.2}
+    //else -> game over
+  }
+}
+
+
+//engine 
+
+engine.addSystem(new MoveObstacle())
+engine.addSystem(new MoveBird())
+
+engine.addEntity(obstacle)
+engine.addEntity(cube)
+engine.addEntity(roof)
+engine.addEntity(collider)
+
